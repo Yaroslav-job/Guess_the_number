b88c46acf1906d5c7ce3351bc08153460acdd251
Yaroslav-job <sarnatsky.yaroslav@gmail.com>
2025-03-28 17:44:53 +0300

    Add files via upload

--- game.go
+++ game.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func play_game() {
 	hidden_num := random()
-	fmt.Println("Число загаданно")
+	fmt.Println("Загаданно число от 1 до 100")
 	attempts := check_num(hidden_num)
 	fmt.Printf("\nВы использовали %d попыток", attempts)
-	play := play_again()
-	if play == 1 {
-		main()
-	} else {
-		fmt.Println("Спасибо за игру")
-	}
+	game_over()
 }
 
-func play_again() int {
+func game_over() {
 	var play_again int
 	fmt.Println("\nХотите сыграть ещё раз: \n1 - Да \n2 - Нет")
 	for {
 		fmt.Scan(&play_again)
-		if play_again == 1 || play_again == 2 {
-			break
+		if play_again == 1 {
+			main()
+		} else if play_again == 2 {
+			os.Exit(0)
 		} else {
-			fmt.Println("Вы ввели недопустимое число. Попробуйте ещё раз:")
+			fmt.Printf("Вы ввели недопустимое число. Попробуйте ещё раз:")
 		}
 	}
-	return play_again
 }
